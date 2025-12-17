875fd1ac96fda49032661b4d0fb3ea5f58c72555
madeval <alvarez.fing@gmail.com>
2025-12-17 00:14:02 -0300

    Optimize DOM rendering using DocumentFragment in fetch-data.js

--- 02-ejercicio-javascript/fetch-data.js
+++ 02-ejercicio-javascript/fetch-data.js
@@ -7,6 +7,14 @@ fetch('data.json')
     .then(response => response.json())
     .then((jobs) => {
         console.log(jobs);
+
+        /* 
+        createDocumentFragment() lo que hace es crear un contenedor en memoria que sirve para almacenar todos los elementos del DOM que queremos pintar.
+        Para que sirve esto? Para evitar re dibujar el HTML cada vez que insertamos un elemento dentro del forEach. Lo que hacemos es: agregamos los elementos en el contenedor virtual, y una vez estén todos, pintamos de una sola vez lo que hay en el contenedor sobre el DOM.
+        Esto mejora bastante el rendimiento, sobre todo cuando tenemos muchos elementos :)
+        */
+        const documentFragment = document.createDocumentFragment()
+
         jobs.forEach((job) => {
             const listElement = document.createElement('li');
             const article = document.createElement('article');
@@ -25,7 +33,9 @@ fetch('data.json')
                     <button class="button-apply-job">Aplicar</button>
             `;
             listElement.appendChild(article);
-            container.appendChild(listElement);
+            documentFragment.appendChild(listElement);
         });
+        
+        container.appendChild(documentFragment);
     })
     .catch(error => console.error('Error fetching data:', error.message));
