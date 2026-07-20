387698a8f6d26b29959d6a145d36e72979b00434
Juan de la Garza <jgarzadi@gmail.com>
2026-07-19 19:12:25 -0600

    Fixed missing 'type' on import.

--- 10-ejercicio-typescript/arrays.ts
+++ 10-ejercicio-typescript/arrays.ts
@@ -1,5 +1,5 @@
 /* Aquí deberás definir los tipos Job y Candidate creados en objects.ts para los arrays dados */
-import { Job, Candidate, Company } from './objects.ts'
+import type { Job, Candidate, Company } from './objects.ts'
 
 export const jobs: Job[] = [
   {
