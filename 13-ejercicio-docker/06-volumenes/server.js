b64ab4fdd8b86ab52c542bb525cee384c8393010
Juan de la Garza <jgarzadi@gmail.com>
2026-09-02 17:57:12 -0600

    6th exercise: volumes and bind mount

--- 13-ejercicio-docker/06-volumenes/server.js
+++ 13-ejercicio-docker/06-volumenes/server.js
@@ -34,7 +34,7 @@ const server = createServer((req, res) => {
   // Ruta por defecto
   let visitasActuales = fs.readFileSync(DATA_FILE, 'utf-8') || '0';
   res.end(JSON.stringify({
-    message: 'HOLA QUÉ PASA CHAVALES',
+    message: 'HOLA QUÉ PASA CHAVALES con mensaje modificado',
     instrucciones: 'Visita /visitar para registrar una nueva visita persistente en disco.',
     contador_actual: parseInt(visitasActuales, 10),
     db_config: `Conectando por red al host de base de datos: "${DB_HOST}"`
